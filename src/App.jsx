dba75a46906a5d762edef1d93dc4a7551a701a62
JABER-HT <81908542+JABER-HT@users.noreply.github.com>
2022-04-30 13:27:59 +0600

    Reaact and Tailwind startup porject file based on Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-blue-900 py-5">
+        <h1 className="text-3xl font-bold text-2lg underline">
+          Hello world!
+        </h1>
+      </header>
+      <div className="bg-red-900">
+        This Is A Div
+      </div>
+    </div>
+  );
+}
+
+export default App;
